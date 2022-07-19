439df5dd27059fa93b826db92b456e9ef743c90f
Donavyn <donavynpence@yahoo.com>
2022-07-19 11:12:10 -0700

    Works, but no error catching, i THINK? will do further testing, may alter back into a .then.catch format

--- java.js
+++ java.js
@@ -18,35 +18,24 @@ let weatherLocation;
 async function getWeatherInfo() {
     const textOutput = document.getElementById('weather-info');
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${weatherLocation}&units=imperial&APPID=d24b077e6c1742ff58872e0136f6ff54`);
+    const responseObj = await response.json()
     const img = document.querySelector('img');
 
-    response.json()
-    .then(function(response) {
-        textOutput.textContent = `Whew! ${response.name}'s Weather sure is something!
-                                    its a whopping ${response.main.temp} degrees 
-                                    farenheit there! oof`;
-        console.log(response);
-        console.log(response.name);
-        console.log(response.main);
-        console.log(response.main.temp);
-        if (response.main.temp < 100) console.log('real hot')
-    })
-    .then(async function (response) {
-        let hotOrCold;
-        if (response.main.temp > 70) {
-            hotOrCold = 'hot';
-        } else {
-            hotOrCold = 'cold';
-        }
-       const gifResponse = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=5J1l9E8XtgAsuQhUkHvL1CnhMZsaNv3f&s=${hotOrCold}`, {mode: 'cors'});
-       
-       gifResponse.json()
-       .then(function(gifResponse) {
-        img.src = gifResponse.data.images.original.url
-       })
-    })
-    .catch(function(err) {
-        console.log(err);
-        console.log('oops something went wrong!')
-    })
+    textOutput.textContent = `Whew! ${responseObj.name}'s Weather sure is something!
+                                its a whopping ${responseObj.main.temp} degrees 
+                                farenheit there! oof`;
+
+    let hotOrCold;
+    
+    if (responseObj.main.temp > 70) {
+        hotOrCold = 'hot';
+        console.log('1')
+    } else {
+        hotOrCold = 'cold';
+        console.log('2')
+    }
+    const gifResponse = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=5J1l9E8XtgAsuQhUkHvL1CnhMZsaNv3f&s=${hotOrCold}`, {mode: 'cors'});
+    const gifResponseObj = await gifResponse.json()
+
+    img.src = gifResponseObj.data.images.original.url
 }
